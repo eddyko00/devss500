9462073646ae720c6c9998efa310e3bd468228e3
koed <koed@L097105.corp.ads>
2020-10-23 13:33:33 -0400

    ver1

--- src/main/resources/static/js/ssnsweb.js
+++ src/main/resources/static/js/ssnsweb.js
@@ -3,6 +3,6 @@
 
 //var iisurl_OP = "http://devssns-project001.paas-app-east-np.tsabcl.tabcelus.com/";
 var iisurl_OP = "https://devssns-ssns-homephonemgmt.paas-app-east-np.tsabcl.tabcelus.com";
-var iisurl_LOCAL = "http://100.67.27.47:8080"; //"http://L097105:8080";
-var iisurl_LOCALHOST = "http://100.67.27.47:8080"; //"http://localhost:8080";
+var iisurl_LOCAL = "http://L097105:8080";
+var iisurl_LOCALHOST = "http://localhost:8080";
 var iisurl = iisurl_OP; 
